a08acb6f10c63e6fcf98f1387620fd2beb15895e
yz <15822426096@163.com>
2020-11-18 15:01:52 +0800

    新增拦截器功能，拦截controller返回值。也可以在访问controller之前进行拦截。

---
+++ springcloud-demo/demo-service/demo-business/src/main/java/com/aiqinhai/demo/interceptor/DemoInterceptor.java
@@ -0,0 +1,42 @@
+package com.aiqinhai.demo.interceptor;
+
+import org.springframework.lang.Nullable;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@ControllerAdvice
+public class DemoInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        System.out.println("prehandle");
+        return true;
+    }
+
+    public void postHandle(WebRequest request, ModelMap model) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler,
+                                @Nullable Exception ex) throws Exception {
+        if(request.getMethod().equalsIgnoreCase("OPTIONS")) {
+            return;
+        }
+        System.out.println(request.getRequestURI().toString());
+        if(request.getRequestURI().equals("/demo-business/demo1")) {
+            try{
+                String result = (String)request.getAttribute("grade");
+                System.out.println("拦截到controller的返回值！！！");
+            }catch(Exception e) {
+                System.out.println("出错了 !!!");
+            }
+        }
+    }
+}
